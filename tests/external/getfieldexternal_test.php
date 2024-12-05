9e9e42c05694dcc73149631cb32f8dfdd058ae36
Bruno Baudry <bruno.baudry@bfh.ch>
2024-12-05 16:39:20 +0100

    prerelease

--- tests/external/getfieldexternal_test.php
+++ tests/external/getfieldexternal_test.php
@@ -18,30 +18,26 @@
  * Test cases for get_translations external.
  *
  * @package    local_deepler
- * @copyright  2024 Bruno Baudry <bruno.baudry@bfh.ch>
+ * @copyright  2024 Bruno Baudry
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @see        https://docs.moodle.org/dev/PHPUnit
  */
 
 namespace local_deepler\external;
-
-use advanced_testcase;
+defined('MOODLE_INTERNAL') || die();
+global $CFG;
+require_once($CFG->dirroot . '/local/deepler/tests/external/base_external.php');
 
 /**
  * Test cases for get_translations external.
  */
-final class getfieldexternal_test extends advanced_testcase {
-
-    public function setUp(): void {
-        parent::setUp();
-        $this->setAdminUser();
-
-    }
+final class getfieldexternal_test extends base_external {
 
     /**
-     * Tests the execution of the get_filed external.
+     * Tests the execution of the get_field external.
      *
-     * @covers \local_deepler\external\get_field::execute
+     * @dataProvider execute_provider
+     * @covers       \local_deepler\external\get_field::execute
      * @return void
      * @throws \core_external\restricted_context_exception
      * @throws \dml_exception
@@ -49,44 +45,29 @@ final class getfieldexternal_test extends advanced_testcase {
      * @throws \invalid_parameter_exception
      * @throws \required_capability_exception
      * @throws \coding_exception
-     * @todo MDL-0 finish testing module manage cap.
      */
-    public function test_execute(): void {
-        global $DB, $CFG;
-        $this->resetAfterTest(true);
-        // Skip this test for Moodle versions lower than 4.0.2.
-        if (version_compare($CFG->version, '2023042400', '<')) {
-            $this->markTestSkipped('This test is only for Moodle 4.0.2 and above.');
+    public function test_execute($field): void {
+        if ($this->is_below_four_one()) {
             return;
         }
-        // Create a test course.
-        $course = $this->getDataGenerator()->create_course();
 
-        // Create a test user with necessary capabilities.
-        $user = $this->getDataGenerator()->create_user();
-        $this->getDataGenerator()->enrol_user($user->id, $course->id, 'editingteacher');
-        $this->setUser($user);
-
-        // Grant the necessary capability.
-        $context = \context_course::instance($course->id);
-        $roleid = $this->getDataGenerator()->create_role();
-        assign_capability('local/deepler:edittranslations', CAP_ALLOW, $roleid, $context);
-        role_assign($roleid, $user->id, $context);
+        list($course, $user) = $this->create_test_course_and_user();
+        $this->grant_capability($user, $course);
 
         // Create a test activity (page).
-        $page = $this->getDataGenerator()->create_module('page',
-                [
-                        'course' => $course->id,
-                        'name' => 'Test Page',
-                        'content' => 'Test content',
-                ]);
+        $page = $this->getDataGenerator()->create_module('page', [
+                'course' => $course->id,
+                'name' => 'Test Page',
+                'content' => 'Test content',
+        ]);
+
         // Prepare test data.
         $data = [
                 [
                         'courseid' => $course->id,
                         'id' => $course->id,
                         'table' => 'course',
-                        'field' => 'fullname',
+                        'field' => $field,
                 ],
         ];
 
@@ -95,13 +76,38 @@ final class getfieldexternal_test extends advanced_testcase {
 
         // Assert the results.
         $this->assertCount(1, $result);
-        $this->assertEquals($course->fullname, $result[0]['text']);
+        $this->assertEquals($course->{$field}, $result[0]['text']);
     }
 
     /**
-     * Test if an error is thrown when cap is not granted.
+     * Data provider for test_execute.
      *
-     * @covers \local_deepler\external\get_field::execute
+     * @return array
+     */
+    public static function execute_provider(): array {
+        return [
+                ['fullname'],
+                ['shortname'],
+        ];
+    }
+
+    /**
+     * Data provider for test_execute_without_capability.
+     *
+     * @return array
+     */
+    public static function execute_without_capability_provider(): array {
+        return [
+                ['fullname'],
+                ['shortname'],
+        ];
+    }
+
+    /**
+     * Test if an error is thrown when capability is not granted.
+     *
+     * @dataProvider execute_without_capability_provider
+     * @covers       \local_deepler\external\get_field::execute
      * @return void
      * @throws \core_external\restricted_context_exception
      * @throws \dml_exception
@@ -109,51 +115,40 @@ final class getfieldexternal_test extends advanced_testcase {
      * @throws \invalid_parameter_exception
      * @throws \required_capability_exception
      */
-    public function test_execute_without_capability(): void {
-        global $CFG;
-        $this->resetAfterTest(true);
-        // Skip this test for Moodle versions lower than 4.0.2.
-        if (version_compare($CFG->version, '2023042400', '<')) {
-            $this->markTestSkipped('This test is only for Moodle 4.0.2 and above.');
+    public function test_execute_without_capability($field): void {
+        if ($this->is_below_four_one()) {
             return;
         }
-        // Create a test course.
-        $course = $this->getDataGenerator()->create_course();
 
-        // Create a user without the necessary capability.
+        $course = $this->getDataGenerator()->create_course();
         $user = $this->getDataGenerator()->create_user();
         $this->setUser($user);
         $this->getDataGenerator()->enrol_user($user->id, $course->id, 'editingteacher');
-        // Prepare test data.
+
         $data = [
                 [
                         'courseid' => $course->id,
                         'id' => $course->id,
                         'table' => 'course',
-                        'field' => 'fullname',
+                        'field' => $field,
                 ],
         ];
 
-        // Execute the external function and expect an exception.
         $this->expectException(\required_capability_exception::class);
         get_field::execute($data);
     }
 
     /**
-     * Tests execute params.
+     * Tests execute parameters.
      *
      * @covers \local_deepler\external\get_field::execute_parameters
      * @return void
      */
     public function test_execute_parameters(): void {
-        global $CFG;
-        $this->resetAfterTest(true);
-        // Skip this test for Moodle versions lower than 4.0.2.
-        if (version_compare($CFG->version, '2023042400', '<')) {
-            $this->markTestSkipped('This test is only for Moodle 4.0.2 and above.');
+        if ($this->is_below_four_one()) {
             return;
         }
-        $params = update_translation::execute_parameters();
+        $params = get_field::execute_parameters();
         $this->assertInstanceOf(\core_external\external_function_parameters::class, $params);
     }
 
@@ -164,14 +159,10 @@ final class getfieldexternal_test extends advanced_testcase {
      * @return void
      */
     public function test_execute_returns(): void {
-        global $CFG;
-        $this->resetAfterTest(true);
-        // Skip this test for Moodle versions lower than 4.0.2.
-        if (version_compare($CFG->version, '2023042400', '<')) {
-            $this->markTestSkipped('This test is only for Moodle 4.0.2 and above.');
+        if ($this->is_below_four_one()) {
             return;
         }
-        $returns = update_translation::execute_returns();
+        $returns = get_field::execute_returns();
         $this->assertInstanceOf(\core_external\external_multiple_structure::class, $returns);
     }
 }
--- tests/external/updatetranslation_test.php
+++ tests/external/updatetranslation_test.php
@@ -18,41 +18,29 @@
  * Test cases for update_translations external.
  *
  * @package    local_deepler
- * @copyright  2024 Bruno Baudry <bruno.baudry@bfh.ch>
+ * @copyright  2024 Bruno Baudry
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @see        https://docs.moodle.org/dev/PHPUnit
  */
 
 namespace local_deepler\external;
-
-use advanced_testcase;
+defined('MOODLE_INTERNAL') || die();
+global $CFG;
+require_once($CFG->dirroot . '/local/deepler/tests/external/base_external.php');
 
 /**
  * Test cases for update_translations external.
  */
-final class updatetranslation_test extends advanced_testcase {
-    /**
-     * Set up.
-     *
-     * @return void
-     */
-    public function setUp(): void {
-        parent::setUp();
-        $this->setAdminUser();
-    }
+final class updatetranslation_test extends base_external {
 
     /**
-     * Tests execute params.
+     * Tests execute parameters.
      *
      * @covers \local_deepler\external\update_translation::execute_parameters
      * @return void
      */
     public function test_execute_parameters(): void {
-        global $CFG;
-        $this->resetAfterTest(true);
-        // Skip this test for Moodle versions lower than 4.0.2.
-        if (version_compare($CFG->version, '2023042400', '<')) {
-            $this->markTestSkipped('This test is only for Moodle 4.0.2 and above.');
+        if ($this->is_below_four_one()) {
             return;
         }
         $params = update_translation::execute_parameters();
@@ -60,17 +48,13 @@ final class updatetranslation_test extends advanced_testcase {
     }
 
     /**
-     *  Tests execute returns.
+     * Tests execute returns.
      *
      * @covers \local_deepler\external\update_translation::execute_returns
      * @return void
      */
     public function test_execute_returns(): void {
-        global $CFG;
-        $this->resetAfterTest(true);
-        // Skip this test for Moodle versions lower than 4.0.2.
-        if (version_compare($CFG->version, '2023042400', '<')) {
-            $this->markTestSkipped('This test is only for Moodle 4.0.2 and above.');
+        if ($this->is_below_four_one()) {
             return;
         }
         $returns = update_translation::execute_returns();
@@ -78,9 +62,22 @@ final class updatetranslation_test extends advanced_testcase {
     }
 
     /**
-     * Tests update_translation successs.
+     * Data provider for test_execute_success.
      *
-     * @covers \local_deepler\external\update_translation::execute
+     * @return array
+     */
+    public static function execute_success_provider(): array {
+        return [
+                ['{mlang en}New Course Name{mlang}{mlang de}Neuer Kursname}'],
+                ['{mlang en}Updated Course Name{mlang}{mlang fr}Nom du cours mis à jour}'],
+        ];
+    }
+
+    /**
+     * Tests update_translation success.
+     *
+     * @dataProvider execute_success_provider
+     * @covers       \local_deepler\external\update_translation::execute
      * @return void
      * @throws \coding_exception
      * @throws \core_external\restricted_context_exception
@@ -89,29 +86,15 @@ final class updatetranslation_test extends advanced_testcase {
      * @throws \invalid_parameter_exception
      * @throws \required_capability_exception
      */
-    public function test_execute_success(): void {
-        global $DB, $CFG;
-        $this->resetAfterTest(true);
-        // Skip this test for Moodle versions lower than 4.0.2.
-        if (version_compare($CFG->version, '2023042400', '<')) {
-            $this->markTestSkipped('This test is only for Moodle 4.0.2 and above.');
+    public function test_execute_success($newcoursename): void {
+        global $DB;
+        if ($this->is_below_four_one()) {
             return;
         }
-        // Create a test course.
-        $course = $this->getDataGenerator()->create_course();
 
-        // Create a test user with necessary capabilities.
-        $user = $this->getDataGenerator()->create_user();
-        $this->getDataGenerator()->enrol_user($user->id, $course->id, 'editingteacher');
-        $this->setUser($user);
+        list($course, $user) = $this->create_test_course_and_user();
+        $this->grant_capability($user, $course);
 
-        // Grant the necessary capability.
-        $context = \context_course::instance($course->id);
-        $roleid = $this->getDataGenerator()->create_role();
-        assign_capability('local/deepler:edittranslations', CAP_ALLOW, $roleid, $context);
-        role_assign($roleid, $user->id, $context);
-
-        // Insert a test record in local_deepler table.
         $deeplerrecord = (object) [
                 't_id' => $course->id,
                 't_lang' => 'en',
@@ -122,21 +105,17 @@ final class updatetranslation_test extends advanced_testcase {
         ];
         $deeplerid = $DB->insert_record('local_deepler', $deeplerrecord);
 
-        // Prepare test data.
         $data = [[
                 'courseid' => $course->id,
                 'id' => $course->id,
                 'tid' => $deeplerid,
                 'table' => 'course',
                 'field' => 'fullname',
-                'text' => '{mlang en}New Course Name{mlang}{mlang de}Neuer Kursname}',
-        ],
-        ];
+                'text' => $newcoursename,
+        ]];
 
-        // Execute the external function.
         $result = update_translation::execute($data);
 
-        // Assert the results.
         $this->assertCount(1, $result);
         $this->assertArrayHasKey('t_lastmodified', $result[0]);
         $this->assertArrayHasKey('text', $result[0]);
@@ -144,19 +123,30 @@ final class updatetranslation_test extends advanced_testcase {
         $this->assertEquals('course-' . $course->id . '-fullname', $result[0]['keyid']);
         $this->assertEquals($data[0]['text'], $result[0]['text']);
 
-        // Check if the course record was updated.
         $updatedcourse = $DB->get_record('course', ['id' => $course->id]);
         $this->assertEquals($data[0]['text'], $updatedcourse->fullname);
 
-        // Check if the local_deepler record was updated.
         $updateddeepler = $DB->get_record('local_deepler', ['id' => $deeplerid]);
         $this->assertEquals($result[0]['t_lastmodified'], $updateddeepler->t_lastmodified);
     }
 
     /**
-     *  Tests update_translation failing for wrong cap.
+     * Data provider for test_execute_without_capability.
      *
-     * @covers \local_deepler\external\update_translation::execute
+     * @return array
+     */
+    public static function execute_without_capability_provider(): array {
+        return [
+                ['New Course Name'],
+                ['Updated Course Name'],
+        ];
+    }
+
+    /**
+     * Tests update_translation failing for wrong capability.
+     *
+     * @dataProvider execute_without_capability_provider
+     * @covers       \local_deepler\external\update_translation::execute
      * @return void
      * @throws \core_external\restricted_context_exception
      * @throws \dml_exception
@@ -164,22 +154,16 @@ final class updatetranslation_test extends advanced_testcase {
      * @throws \invalid_parameter_exception
      * @throws \required_capability_exception
      */
-    public function test_execute_without_capability(): void {
-        global $CFG;
-        $this->resetAfterTest(true);
-        // Skip this test for Moodle versions lower than 4.0.2.
-        if (version_compare($CFG->version, '2023042400', '<')) {
-            $this->markTestSkipped('This test is only for Moodle 4.0.2 and above.');
+    public function test_execute_without_capability($newcoursename): void {
+        if ($this->is_below_four_one()) {
             return;
         }
-        // Create a test course.
-        $course = $this->getDataGenerator()->create_course();
 
-        // Create a user without the necessary capability.
+        $course = $this->getDataGenerator()->create_course();
         $user = $this->getDataGenerator()->create_user();
         $this->setUser($user);
         $this->getDataGenerator()->enrol_user($user->id, $course->id, 'editingteacher');
-        // Prepare test data.
+
         $data = [
                 [
                         'courseid' => $course->id,
@@ -187,14 +171,11 @@ final class updatetranslation_test extends advanced_testcase {
                         'tid' => 1,
                         'table' => 'course',
                         'field' => 'fullname',
-                        'text' => 'New Course Name',
+                        'text' => $newcoursename,
                 ],
         ];
 
-        // Execute the external function and expect an exception.
         $this->expectException(\required_capability_exception::class);
-        $result = update_translation::execute($data);
-        $this->assertArrayHasKey('error', $result[0]);
-        $this->assertStringContainsString('required_capability_exception', $result[0]['error']);
+        update_translation::execute($data);
     }
 }
