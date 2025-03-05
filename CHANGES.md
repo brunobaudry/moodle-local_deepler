b526eda6184303b03020855bc2a60cf5b2d298e4
Bruno Baudry <bruno.baudry@bfh.ch>
2025-03-05 22:41:59 +0100

    Release: v1.3.4

--- CHANGES.md
+++ CHANGES.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.3.0] 2025-03-03
+## [1.3.4] 2025-03-05
 
 ### Added
 - Glossary ids are now saved in cookies (for 1 month) by course and source-target language pairs. As reported with issue #56.
--- version.php
+++ version.php
@@ -29,10 +29,10 @@ if (!isset($plugin)) {
     $plugin = new stdClass();
 }
 $plugin->component = 'local_deepler'; // Full name of the plugin (used for diagnostics).
-$plugin->version = 2025030511; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2025030512; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2020112800; // Requires Moodle 4.1 LTS.
 $plugin->supported = [401, 405]; // Supported Moodle Versions.
 $plugin->maturity = MATURITY_ALPHA; // Maturity level.
-$plugin->release = 'v1.3.3'; // Semantic Versioning for CHANGES.md.
+$plugin->release = 'v1.3.4'; // Semantic Versioning for CHANGES.md.
 // Dependencies.
 $plugin->dependencies = ['filter_multilang2' => 2020101300];
