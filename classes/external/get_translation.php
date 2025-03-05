d3ba5fc50ba567adf2ac19cd98f4955751f99103
Bruno Baudry <bruno.baudry@bfh.ch>
2025-03-05 22:10:08 +0100

    added MOODLE_INTERNAL check

--- classes/external/get_translation.php
+++ classes/external/get_translation.php
@@ -15,7 +15,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace local_deepler\external;
-require_once(__DIR__ . '/../vendor/autoload.php');
+defined('MOODLE_INTERNAL') || die();
+global $CFG;
+require_once($CFG->dirroot . '/local/deepler/classes/vendor/autoload.php');
 
 use core_external\external_api;
 use core_external\external_function_parameters;
