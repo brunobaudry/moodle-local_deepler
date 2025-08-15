9b0f6064db315ec67dac302ff2bb0f09aff5eebc
Bruno Baudry <bruno.baudry@bfh.ch>
2025-08-15 09:37:14 +0200

    Release: v1.9.1

--- classes/lib/admin_setting_deeplapikey_configtext.php
+++ classes/lib/admin_setting_deeplapikey_configtext.php
@@ -35,7 +35,7 @@ class admin_setting_deeplapikey_configtext extends admin_setting_configtext {
      * @return string|\lang_string|true true if ok string if error found
      */
     public function validate($data): string|lang_string|true {
-
+        global $CFG;
         // Skip validation during install or PHPUnit init.
         if (empty($CFG->version) || during_initial_install()) {
             return true;
