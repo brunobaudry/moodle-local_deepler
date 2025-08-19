02a0a23a136610fe7bdf23c54041f7c82b13152c
Bruno Baudry <bruno.baudry@bfh.ch>
2025-08-19 11:04:06 +0200

    Release: v1.9.4

--- version.php
+++ version.php
@@ -30,10 +30,10 @@ if (!isset($plugin)) {
 }
 $plugin->component = 'local_deepler'; // Full name of the plugin (used for diagnostics).
 $plugin->settings = true;
-$plugin->version = 2025081801; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2025081900; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2023042400; // Requires Moodle 4.2.2.
 $plugin->supported = [402, 500]; // Supported Moodle Versions.
 $plugin->maturity = MATURITY_BETA; // Maturity level.
-$plugin->release = 'v1.9.3'; // Semantic Versioning for CHANGES.md.
+$plugin->release = 'v1.9.4'; // Semantic Versioning for CHANGES.md.
 // Dependencies.
 $plugin->dependencies = ['filter_multilang2' => 2020101300];
