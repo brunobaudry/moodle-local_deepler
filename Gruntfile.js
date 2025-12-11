ebab1bb8d3e0137d8d538881ba6df421cfbeba91
Bruno Baudry <bruno.baudry@bfh.ch>
2025-12-11 21:19:34 +0100

    back and forth

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,17 @@
 /* eslint no-undef: "error"*/
 module.exports = function(grunt) {
 
+    // Load the core Moodle Gruntfile for AMD tasks
+    const path = require("path");
+    const fs = require("fs");
+    // eslint-disable-next-line no-undef
+    const moodleGruntfile = path.resolve(__dirname, "../../../Gruntfile.js");
+
+    if (fs.existsSync(moodleGruntfile)) {
+        require("grunt-load-gruntfile")(grunt);
+        grunt.loadGruntfile(moodleGruntfile);
+    }
+
     // Load all grunt tasks.
     grunt.loadNpmTasks("grunt-contrib-sass");
     grunt.loadNpmTasks("grunt-contrib-watch");
@@ -38,19 +49,6 @@ module.exports = function(grunt) {
                 }
             }
         }
-    });
-
-    // Register empty tasks for moodle-plugin-ci compatibility
-    grunt.registerTask("amd", "AMD task not needed for this plugin", function() {
-        grunt.log.ok("No AMD modules to process.");
-    });
-
-    grunt.registerTask("stylelint", "Stylelint task not needed for this plugin", function() {
-        grunt.log.ok("No custom styles to lint.");
-    });
-
-    grunt.registerTask("ignorefiles", "Ignore files task not needed for this plugin", function() {
-        grunt.log.ok("No ignore files to check.");
     });
 
     // The default task (running "grunt" in the console).
