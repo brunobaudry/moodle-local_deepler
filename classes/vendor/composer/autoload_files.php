85af12181e40beb02063df3f436315413d6c4f03
Bruno Baudry <bruno.baudry@bfh.ch>
2026-01-12 14:26:34 +0100

    updated Deepl dependecies

--- classes/vendor/composer/autoload_files.php
+++ classes/vendor/composer/autoload_files.php
@@ -8,5 +8,7 @@ $baseDir = dirname(dirname($vendorDir));
 return array(
     '6e3fae29631ef280660b3cdad06f25a8' => $vendorDir . '/symfony/deprecation-contracts/function.php',
     '2cffec82183ee1cea088009cef9a6fc3' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
     '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
+    'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
 );
